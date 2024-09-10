7c9d17786a6b72ecd96d18c80bdb31a8b8b8dc3d
LuisEdua <lefarrera@gmail.com>
2024-09-10 08:33:33 -0600

    [UPDATE] app.py

--- src/app.py
+++ src/app.py
@@ -9,18 +9,6 @@ app.register_blueprint(openai_controller, url_prefix='/openai')
 
 CORS(app)
 
-def main():
-    cloudinary_module = CloudinaryModule()
-
-    # Example usage: upload an image
-    try:
-        with open('/Users/keyel/Documents/ChatBotNeurotry/src/phone.png', 'rb') as image_file:  # Replace with your image file name
-            image_stream = BytesIO(image_file.read())
-            result = cloudinary_module.cloudinary_service.upload_image(image_stream)
-            print(result)
-    except FileNotFoundError as e:
-        print(f"Error: {e}")
-
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
