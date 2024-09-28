e009913276e04aa9afb5a2b2df87de35c94d2f79
LuisEdua <lefarrera@gmail.com>
2024-09-27 20:07:56 -0600

    [UPDATE] Sumary

--- src/whatsapp/whatsapp_service.py
+++ src/whatsapp/whatsapp_service.py
@@ -81,12 +81,10 @@ class WhatsappService:
         client_service = await self.model.evaluate_client_response(message["text"]["body"].lower())
         await self.save_message(message["text"]["body"], message["id"], message["from"],
                                 client_service["segmentations"], client_service["user_profile_data"])
-        if client_service['isSummary']:
-            pass
-        if client_service['isLogin']:
-            await self.handle_login(message)
-        elif client_service['isWelcome']:
+        if client_service['isWelcome']:
             await self.is_welcome(message)
+        elif client_service['isSummary']:
+            await self.sumary(message)
         elif client_service['wantToBuy'] and not client_service['catalog']:
             await self.want_to_buy(message)
         elif client_service['isGivingThanks']:
@@ -213,3 +211,9 @@ class WhatsappService:
     async def get_next_screen(self, phone_number: str, intent: str):
         response = await get_next_screen(phone_number, intent)
         return response
+
+    async def sumary(self, message):
+        new_session = session()
+        messages = new_session.query(Message).filter(Message.number == message["from"]).all()
+        sumary = await self.model.generate_sumary(messages)
+        await send_message_fetch(sumary, message["from"])
